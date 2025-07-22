ed059e75ce6c7a670fdb689a9c9b02d335d33875
Hilina Zemedkun <169660585+hilinaz@users.noreply.github.com>
2025-07-22 17:21:17 +0300

    e-commerce ui clone

---
+++ mobile/Hilina_Zemedkun/e_commerce_products/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:e_commerce_products/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
