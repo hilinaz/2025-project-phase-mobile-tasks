b75c2248dd6dc3f2b31e1f7448d46d57bed97401
Hilina Zemedkun <169660585+hilinaz@users.noreply.github.com>
2025-08-02 17:28:53 +0300

    wrong file

--- task_9/e_commerce_app/lib/repositories/product_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/product_entity.dart';
-
-abstract class ProductRepository {
-  Future<void> insertProduct(ProductEntity product);
-  Future<void> updateProduct(ProductEntity product);
-  Future<void> deleteProduct(String id);
-  Future<ProductEntity?> getProduct(String id);
-  Future<List<ProductEntity>> getAllProducts();
-} 
