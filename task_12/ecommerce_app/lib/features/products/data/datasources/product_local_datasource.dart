64af13de731ed58362914804612838508522a8a1
Hilina Zemedkun <169660585+hilinaz@users.noreply.github.com>
2025-08-04 01:48:44 +0300

    task_12 implementation of repository

--- task_12/ecommerce_app/lib/features/products/data/datasources/product_local_datasource.dart
+++ task_12/ecommerce_app/lib/features/products/data/datasources/product_local_datasource.dart
@@ -3,4 +3,10 @@ import '../models/product_model.dart';
 abstract class ProductLocalDatasource {
   Future<ProductModel> getProduct();
   Future<void> cacheProduct(ProductModel productModel);
+
+  Future<void> deleteProduct(String id) async {}
+
+  Future getProductById(String id) async {}
+
+  Future getAllProducts() async {}
 }
